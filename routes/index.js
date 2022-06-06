ec457a6d42ca152d95947f8345aeacfc2b2184f7
hwakyung <hwakyung5531@gmail.com>
2022-06-06 17:13:33 +0900

    Update routes/, schemas/, app.js

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,17 @@
-const express = require('express');
-const testSchema = require('../schemas/photo');
-const mongoose = require('mongoose');
+const express = require("express");
+const pSchema = require("../schemas/photo");
+const tSchema = require("../schemas/tags");
+const mongoose = require("mongoose");
 const router = express.Router();
 
-router.post('/', async (req, res) => {
+router.post("/", async (req, res) => {
     try{
         const UID = req.body.tname;
         console.log(UID);
         await mongoose.model(UID, testSchema, UID).create({
             id: "testing"
         });
-        res.send({'respose': 21011});
+        res.send({"respose": 21011});
     
     } catch(err) {
         console.error(err);
--- schemas/index.js
+++ schemas/index.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const connect = () => {
     mongoose.connect(process.env.MONGO_URI, {
