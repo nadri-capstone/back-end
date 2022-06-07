d7f8c7f69ba1d0991a704164b09329d7bec3ed73
hwakyung <hwakyung5531@gmail.com>
2022-06-07 16:55:26 +0900

    Update routes/index.js

--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,13 @@ const tSchema = require("../schemas/tags");
 const mongoose = require("mongoose");
 const router = express.Router();
 
-router.get("/", async (req, res) => {
+router.get("/:id", async (req, res) => {
     try{
-        const android_id = req.body.android_id;
-        const result = await mongoose.model(android_id, pSchema, android_id).find({
-        });
+        const android_id = req.params.id;
+        console.log(android_id);
+        const result = await mongoose.model("android", pSchema, "android").find({});
+        console.log(typeof(android_id));
+        res.json(result);
 
     } catch(err) {
         console.error(err);
@@ -19,8 +21,10 @@ router.post("/", async (req, res) => {
     try{
         const android_id = req.body.android_id;
         console.log(android_id);
-        await mongoose.model(android_id, pSchema, android_id).create({
+        const time_data = req.body.time_data;
+        await mongoose.model("android", pSchema, "android").create({
             id: "testing",
+            data: Date(req.body.time_data),
             tags: {
                 tag: "Dog", 
                 odj: "20, 21"
