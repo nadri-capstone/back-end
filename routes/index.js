698c336df7e7523527bdf1906605fadfe7d28e3f
hwakyung99 <58030720+hwakyung99@users.noreply.github.com>
2022-06-20 00:32:24 +0900

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,18 @@ const tSchema = require("../schemas/tags");
 const mongoose = require("mongoose");
 const router = express.Router();
 
-router.get("/:id", async (req, res) => {
+router.get("/:id/:date", async (req, res) => {
     try{
         const android_id = req.params.id;
         console.log(android_id);
-        const result = await mongoose.model(android_id, pSchema, android_id).find({});
-        console.log(typeof(android_id));
+        var datetime1 = new Date(req.params.date);
+        var datetime2 = new Date(req.params.date);
+        datetime2.setDate(datetime1.getDate() + 1);
+        console.log(datetime1, datetime2);
+        const result = await mongoose.model(android_id, pSchema, android_id).find({
+            datetime : {$gte: datetime1, $lte: datetime2}
+        });
+        console.log(result);
         res.json(result);
 
     } catch(err) {
@@ -21,18 +27,32 @@ router.post("/:id", async (req, res) => {
     try{
         const android_id = req.params.id;
         console.log(android_id);
-        var body = req.body;
-        console.log(body);
-         for(idx in body){
-            console.log(body[idx]);
-             const photo = body[idx];
-            await mongoose.model(android_id, pSchema, android_id).create({
-                id: photo.id,
-                datetime: Date(photo.datetime),
-                location: photo.location,
-            });
+        
+        console.log(req.body);
+
+        //사진들
+        const photos = req.body.photos;
+
+        //저장여부
+        var existed = req.body.existed;
+        existed = existed.slice(1, -1);
+        existed = existed.split(', ');
+        console.log(existed);
+        
+        //사진 저장
+        for(idx in photos){
+            console.log(photos[idx], existed[idx]);
+            const photo = photos[idx];
+            if(existed[idx] == "false"){
+                await mongoose.model(android_id, pSchema, android_id).create({
+                    id: photo.id,
+                    datetime: Date(photo.datetime),
+                    location: photo.location,
+                });
+            }
         };
-        res.send([{"respose": "isSuccess"}]);
+
+        res.send({"respose": "isSuccess"});
     
     } catch(err) {
         console.error(err);
